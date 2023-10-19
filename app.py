e47d947975dab936692f6945a900b7f464507974
Isomorphismss <jw6699@nyu.edu>
2023-10-18 20:47:34 -0400

    Multiple bugs fixed, removed the CSS to ease the implementation, updated the log-in logic

--- app.py
+++ app.py
@@ -1,28 +1,51 @@
-from flask import Flask, render_template, request, redirect, abort, url_for, make_response
 import pymongo
+from bson.objectid import ObjectId
+from flask import Flask, render_template, request, flash, redirect, abort, url_for, make_response
+import datetime
 
 app = Flask(__name__)
 
-try:
-    connection = pymongo.MongoClient('mongodb://admin:secret@localhost:27017')
-    connection.server_info()
-except pymongo.errors.ServerSelectionTimeoutError as err:
-    print("Could not connect to MongoDB:", err)
-    connection = None
-db = connection["Isomorphism"] if connection else None
+client = pymongo.MongoClient('mongodb://admin:secret@localhost:27017')
+db = client["Isomorphism"]  
+app.secret_key = "supersecretkey"
+
+@app.route('/')
+def index_page():
+    return render_template('index.html')
+
+@app.route('/register')
+def register_page():
+    return render_template('register.html')
+
+@app.route('/donor_home')
+def donor_home_page():
+    return render_template('donor_home.html')
 
 @app.route('/register', methods=['POST'])
 def register():
-    if not db: 
-        return "Error: Database connection failed!", 500
-    user_type = request.form['userType']
     username = request.form['username']
     password = request.form['password']
-    if user_type == "donor":
-        db.donors.insert_one({"username": username, "password": password})
-    elif user_type == "charity":
-        db.charities.insert_one({"username": username, "password": password})
-    return "Registration successful!"
+    db.donors.insert_one({"username": username, "password": password})
+    return render_template('success.html')
+
+@app.route('/login', methods=['GET'])
+def display_login():
+    return render_template('index.html')
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.form['login-username']
+    password = request.form['login-password']
+    donor = db.donors.find_one({"username": username})
+    if donor:
+        if donor["password"] == password:
+            flash('Login successful!')
+            return redirect(url_for('donor_home_page'))
+        else:
+            return render_template('index.html', error = "Incorrect password.")
+    else:
+        return render_template('index.html', error = "Usernname Not Found.")
+
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
