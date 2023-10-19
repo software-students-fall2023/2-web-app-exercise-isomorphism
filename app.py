91d8187dd4998022913dc3605544bdd3f660be27
Isomorphismss <jw6699@nyu.edu>
2023-10-18 21:36:24 -0400

    Added options for user to log-in/register as a charity or a donor, several bug fixes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import pymongo
 from bson.objectid import ObjectId
-from flask import Flask, render_template, request, flash, redirect, abort, url_for, make_response
+from flask import Flask, render_template, request, redirect, abort, url_for, make_response
 import datetime
 
 app = Flask(__name__)
@@ -21,11 +21,19 @@ def register_page():
 def donor_home_page():
     return render_template('donor_home.html')
 
+@app.route('/charity_home')
+def charity_home_page():
+    return render_template('charity_home.html')
+
 @app.route('/register', methods=['POST'])
 def register():
     username = request.form['username']
     password = request.form['password']
-    db.donors.insert_one({"username": username, "password": password})
+    user_type = request.form['user_type']
+    if user_type == "donor":
+        db.donors.insert_one({"username": username, "password": password})
+    elif user_type == "charity":
+        db.charities.insert_one({"username": username, "password": password})
     return render_template('success.html')
 
 @app.route('/login', methods=['GET'])
@@ -36,15 +44,26 @@ def display_login():
 def login():
     username = request.form['login-username']
     password = request.form['login-password']
-    donor = db.donors.find_one({"username": username})
-    if donor:
-        if donor["password"] == password:
-            flash('Login successful!')
-            return redirect(url_for('donor_home_page'))
+    user_type = request.form['login-user_type']
+    if user_type == "donor":
+        donor = db.donors.find_one({"username": username})
+        if donor:
+            if donor["password"] == password:
+                return redirect(url_for('donor_home_page'))
+            else:
+                return render_template('index.html', error = "Incorrect password.")
         else:
-            return render_template('index.html', error = "Incorrect password.")
-    else:
-        return render_template('index.html', error = "Usernname Not Found.")
+            return render_template('index.html', error = "Usernname Not Found.")
+    elif user_type == "charity":
+        charity = db.charities.find_one({"username": username})
+        if charity:
+            if charity["password"] == password:
+                return redirect(url_for('charity_home_page'))
+            else:
+                return render_template('index.html', error = "Incorrect password.")
+        else:
+            return render_template('index.html', error = "Usernname Not Found.")
+    
 
 
 if __name__ == "__main__":
